f25dd764e9a9d1a83d73a760bf3fcec8c10dd68e
SlushUwU <105487011+SlushUwU@users.noreply.github.com>
2024-04-27 19:23:41 +0200

    Finished Basic Setup

--- bot.js
+++ bot.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 // Register the Libs
-const {Client,  GatewayIntentBits} = require('discord.js');
-const {} = require('products.json');
+const {Client,  GatewayIntentBits, Events} = require('discord.js');
+const {} = require(__dirname+'/products.json');
 const config = process.env;
 
 
 const bot = new  Client({intents: [GatewayIntentBits.Guilds]})
 
+bot.once(Events.ClientReady, readyClient => {
+    console.log(`The bot is running as ${readyClient.user.username}`)
+})
 
 
 bot.login(config.DISCORD_BOT_TOKEN).then(r => {
