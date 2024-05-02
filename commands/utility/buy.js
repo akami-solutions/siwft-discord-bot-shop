68eba1b6517e070147dd1642171c3b889f919120
Akama Aka <rology@satowa-network.eu>
2024-05-02 15:51:22 +0200

    Started with StringOptions

--- commands/utility/buy.js
+++ commands/utility/buy.js
@@ -2,7 +2,10 @@ const {SlashCommandBuilder} = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('buy')
-        .setDescription('Buy products via our Discord Bot'),
+        .setDescription('Buy products via our Discord Bot')
+        .addStringOption(option => {
+
+        }),
     execute(interaction) {
         const {readFileSync} = require('node:fs');
         let products = readFileSync(__dirname+'/../../products.json', {encoding: 'utf-8', flag: 'r'});
