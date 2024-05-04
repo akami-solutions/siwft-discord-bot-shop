30a0193ec71398ff918d08c1be904fbb9150b566
akama-aka <akama.aka@akami-solutions.cc>
2024-05-04 22:30:35 +0200

    Add payment functionality and new product to discord bot
    Implement a new function, createPayment, that interacts with the SIWFT API for financial transactions and updated the buy command in Discord bot to utilize this function. Also, a new product was added to the products.json file.
    
    Signed-off-by: Akama Aka <rology@satowa-network.eu>

--- commands/utility/buy.js
+++ commands/utility/buy.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { readFileSync } = require('fs');
+const {createPayment} = require(__dirname+"/../../functions/siwft_api");
 
 let products = readFileSync(__dirname + '/../../products.json', { encoding: 'utf-8', flag: 'r' });
 products = JSON.parse(products);
@@ -24,11 +25,36 @@ module.exports = {
         .setName('buy')
         .setDescription('Buy products via our Discord Bot')
         .addStringOption(option =>
-            option.setName("product")
+            option.setName("product") // This is the Product ID
                 .setDescription("Please choose a product")
                 .setRequired(true)
-                .addChoices(...getProductObjects(products))),
+                .addChoices(...getProductObjects(products)) // The ... is VERY important here! It's called the spread operator, and it's used to spread the array into individual elements
+        )
+        .addStringOption(option =>
+            option.setName("username") // This is the Username of the User
+                .setDescription("Please enter your Username")
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName("iwan") // This is the IWAN of the User
+                .setDescription("Please enter your IWAN")
+                .setRequired(true)
+        )
+        .addStringOption(option =>
+            option.setName("token") // This is the API Key of the User. IDK why I thought it was a good idea to use the API key for this but that will change with the SIWFT API '^^
+                .setDescription("Please enter your token")
+                .setRequired(true)
+        ),
     async execute(interaction) {
-        await interaction.reply(interaction.options.get('product').value);
+        // Get the selected product and get then the price from the products.json and then create the payment
+
+        // Create the payment Sender IWAN, Receiver IWAN, Amount, Description, Key, Username
+        const payment = await createPayment(interaction.options.get('iwan').value, "AT10000008414712856461", 2, interaction.options.get('product').value, interaction.options.get('token').value, interaction.options.get('username').value);
+        // If the payment was successful, reply with the product ID
+        if (payment.success)
+            // Make the Message just visible to the user who executed the command
+            interaction.reply({content: "Payment successful! Your product ID is: " + interaction.options.get('product').value+"\nPayment ID: "+payment.id, ephemeral: true}); // Prob. I'll add a better Message Formatting :3
+        else
+            interaction.reply({content: "\"Payment failed! Please try again later.", ephemeral: true});
     }
 };
